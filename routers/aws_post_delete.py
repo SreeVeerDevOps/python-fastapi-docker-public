46a1476a6d1fb7331a08b8f8673105700dbe1c16
Sreeharsha Veerapalli :: Kshema <sreeharsha.v@kshema.co>
2025-08-25 15:10:22 +0530

    Developing Post & Delete

--- routers/aws_post_delete.py
+++ routers/aws_post_delete.py
@@ -23,4 +23,18 @@ def post_health():
 def create_item(item: Item):
     return item
 
+@router.post("/items2/", tags=["AWS-POST-DELETE"])
+def create_item2(item: Item):
+    keys = []
+    values = []
+    for k,v in item.items():
+        keys.append(k)
+        values.append(v)
+    return keys
+
+@router.get('/post/certs/{region}', tags=["AWS-RAW"])
+def get_certs(request: Request, region: str):
+    acm_conn = boto3.client('acm',region_name=region)
+    all_certs = acm_conn.list_certificates().get('CertificateSummaryList')
+    return all_certs
 
