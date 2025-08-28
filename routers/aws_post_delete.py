85de37d9a9b2833256fe2872b8e9841d4c1ab23f
SreeVeerDevOps <47476473+SreeVeerDevOps@users.noreply.github.com>
2025-08-28 17:26:42 +0530

    item

--- routers/aws_post_delete.py
+++ routers/aws_post_delete.py
@@ -37,15 +37,9 @@ def create_fake_item():
         raise HTTPException(status_code=400, detail=e.response['Error']['Message'])
 
 @router.post("/items/np/", tags=["DynamoDB"])
-def create_fake_new_person(data):
-    print(data)
-    # Generate fake item data
-    # try:
-    #     # Post fake item to DynamoDB
-    #     table.put_item(Item=item)
-    #     return {"message": "Fake item created", "item": item}
-    # except ClientError as e:
-    #     raise HTTPException(status_code=400, detail=e.response['Error']['Message'])
+def create_item_np(item: Item):
+    print(item)
+    return item
 
 @router.get("/items/{item_id}", tags=["DynamoDB"])
 def get_item(item_id: str):
